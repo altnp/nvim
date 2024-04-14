110b7731d06e75c19bf23f2706213642fe3646b0
Nick Alt <altnp291@gmail.com>
2024-04-13 23:41:04 -0400

    dial

---
+++ lua/plugins/dial.lua
@@ -0,0 +1,55 @@
+return {
+  'monaqa/dial.nvim',
+  keys = {
+    { '<C-a>', mode = 'n', desc = 'Increment' },
+    { '<C-x>', mode = 'n', desc = 'Decrement' },
+    { 'g<C-a>', mode = 'n', desc = 'Decrement' },
+    { 'g<C-x>', mode = 'n', desc = 'Increment' },
+    { '<C-a>', mode = 'v', desc = 'Increment' },
+    { '<C-x>', mode = 'v', desc = 'Decrement' },
+    { 'g<C-a>', mode = 'v', desc = 'Decrement' },
+    { 'g<C-x>', mode = 'v', desc = 'Increment' },
+  },
+  config = function()
+    local augend = require 'dial.augend'
+    require('dial.config').augends:register_group {
+      default = {
+        augend.integer.alias.decimal,
+        augend.integer.alias.hex,
+        augend.date.alias['%Y/%m/%d'],
+        augend.constant.alias.bool,
+        augend.semver.alias.semver,
+        augend.case.new {
+          types = { 'camelCase', 'PascalCase', 'snake_case', 'SCREAMING_SNAKE_CASE' },
+        },
+      },
+    }
+
+    local map = vim.keymap.set
+    map('n', '<C-a>', function()
+      return require('dial.map').manipulate('increment', 'normal')
+    end, { desc = 'Increment' })
+    map('n', '<C-x>', function()
+      return require('dial.map').manipulate('decrement', 'normal')
+    end, { desc = 'Decrement' })
+    map('n', 'g<C-a>', function()
+      return require('dial.map').manipulate('increment', 'gnormal')
+    end, { desc = 'Increment' })
+    map('n', 'g<C-x>', function()
+      return require('dial.map').manipulate('decrement', 'gnormal')
+    end, { desc = 'Decrement' })
+
+    map('x', '<C-a>', function()
+      return require('dial.map').manipulate('increment', 'visual')
+    end, { desc = 'Increment' })
+    map('x', '<C-x>', function()
+      return require('dial.map').manipulate('decrement', 'visual')
+    end, { desc = 'Decrement' })
+    map('x', 'g<C-a>', function()
+      return require('dial.map').manipulate('increment', 'gvisual')
+    end, { desc = 'Increment' })
+    map('x', 'g<C-x>', function()
+      return require('dial.map').manipulate('decrement', 'gvisual')
+    end, { desc = 'Decrement' })
+  end,
+}
